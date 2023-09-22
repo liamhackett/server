19494648b0ee4fb26e0e93a78cbb9469a20dcf37
Liam Hackett <ljh1oo@yahoo.com>
2023-09-22 13:04:18 -0400

    Fix

--- server.js
+++ server.js
@@ -12,13 +12,13 @@ const profile = require("./controllers/profile");
 const image = require("./controllers/image");
 const api = require("./controllers/api");
 
-process.end.NODE_TLS_REJECT_UNAUTHORIZED = 0;
-
 const db = knex({
   client: "pg",
   connection: {
     connectionString: process.env.DATABASE_URL,
-    ssl: true
+    ssl: {
+      rejectUnauthorized: false
+    }
   }
 });
 
